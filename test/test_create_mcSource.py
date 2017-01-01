298e68372e576061ca599d2ff4b0ffc6a420c585
Kyuho Choi <bloodycube@gmail.com>
2017-01-02 08:19:30 +0900

    조정

--- test/test_create_mcSource.py
+++ test/test_create_mcSource.py
@@ -1,17 +1,16 @@
+mocapSrcFile = r'Z:\2016_Dark_Avenger3\B_production\scene\animation\motioncapture\FBX\choice\161227\Etc_orc_attacked_fall_01.fbx'
+#mocapSrcFile = r'Z:\2016_Dark_Avenger3\B_production\scene\animation\motioncapture\FBX\choice\161227\D_01_KO_Fight_knight_look_M_02.fbx'
+outputFoler_motionCapture = 'D:/Users/kyuho_choi/Desktop/motionCapture/motionCapture'
+outputFoler_ainmClip      = 'D:/Users/kyuho_choi/Desktop/motionCapture/animClip'
+outputFolder_playblast    = 'D:/Users/kyuho_choi/Desktop/motionCapture/preview'
+
 mcCharFile = 'D:/Users/kyuho_choi/Desktop/MC/MC.ma'
 jnts = []
 sampleJntName = 'Spine'
-
-#mocapSrcFile = r'Z:\2016_Dark_Avenger3\B_production\scene\animation\motioncapture\FBX\choice\161227\Etc_orc_attacked_fall_01.fbx'
-mocapSrcFile = r'Z:\2016_Dark_Avenger3\B_production\scene\animation\motioncapture\FBX\choice\161227\D_01_KO_Fight_knight_look_M_02.fbx'
 charPrefixs = ['charA','charB','charC','charD','charE','charF','charG','charH']
 mocapNamespace = 'data'
 
-#-----------------
-#
-# 준비
-#
-#-----------------
+# 조인트 리스트
 if not jnts:
     # 새파일
     pm.newFile( force=True )
@@ -23,16 +22,13 @@ if not jnts:
     jnts = [ jnt.name() for jnt in pm.ls(type='joint')]
 
 
-#-----------------
+#---------------
 #
-# 시작
+# Motion Key Transfer
 #
-#-----------------
+#---------------
 # 새파일
 pm.newFile( force=True )
-
-# 씬 이름
-sceneName = pm.mel.basenameEx( mocapSrcFile )
 
 # 레퍼런스로 모션 부름
 refNode = pm.createReference( mocapSrcFile, namespace=mocapNamespace)
@@ -49,27 +45,20 @@ for jnt in pm.ls(type='joint'):
         sampleJnts.append(jnt)
         mocapNamespaces.append(jnt.namespace())
         
-charSets = []
-animClips = []
-for sourceNamespace, prefix in zip(mocapNamespaces,charPrefixs):
+for sourceNamespace, prefix in zip( mocapNamespaces, charPrefixs ):
     #sourceNamespace = mocapNamespaces[0]
     #prefix = charPrefixs[0]
     
+    # 캐릭터 수량에 따른 조정 내용
     importOpt = {"returnNewNodes":True }
     prefix_ = ''
-    subfix_ = ''
     if len(mocapNamespaces)>1 :
         importOpt = {"returnNewNodes":True, "renameAll":True, "renamingPrefix":prefix}
         prefix_ = prefix + '_'
-        subfix_ = '_' + prefix
     
     # 모션캡쳐 캐릭터 파일 임포트
     newNodes = pm.importFile( mcCharFile, **importOpt )
-    
-    # 캐릭터 셋 이름 확인
-    charSet = pm.ls( newNodes, type='character')[0]
-    charSets.append( charSet )
-    
+   
     # 모션캡쳐 조인트에서 키값 복 붙
     errorLine = []
     for jnt in jnts:
@@ -98,14 +87,137 @@ for sourceNamespace, prefix in zip(mocapNamespaces,charPrefixs):
                 # 그냥 값을 입력받아서 적어넣음.
                 target_attr.set( source_attr.get() )
     
-    # 애니메이션 클립 생성
-    animClip = pm.clip(charSet, name='animClip', scheduleClip=True, allAbsolute=True, animCurveRange=True)[0]
-    animClip = pm.PyNode( animClip )
-    animClipName = sceneName
-    if prefix:
-        animClipName = sceneName + subfix_ 
-    animClip.rename( pm.mel.formValidObjectName( animClipName ) )
-    animClips.append(animClip)
-    
 # 레퍼런스 날림
 refNode.remove()
+            
+#---------------
+#
+# animClip
+#
+#---------------
+# 캐릭터 셋 이름 확인
+charSets = pm.ls( type='character')
+animClips = []
+
+for charSet in charSets:
+    # 애니메이션 클립 생성
+    #charSet = charSets[2]
+    animClip = pm.clip(charSet, name='animClip', scheduleClip=True, allAbsolute=True, animCurveRange=True)[0]
+    animClip = pm.PyNode( animClip )
+    
+    # 클립 이름 결정
+    animClipName = pm.mel.basenameEx( mocapSrcFile )
+    if len(charSets)>1 :
+        animClipName = animClipName + '__' + charSet.name().split('_')[0]
+    
+    # 리네임
+    animClip.rename( pm.mel.formValidObjectName( animClipName ) )
+    animClips.append(animClip)
+
+#---------------
+#
+# Export
+#
+#---------------
+# 애니메이션 클립 우선 저장
+for animClip in animClips:
+    exportPath = outputFoler_ainmClip +'/'+ animClip.name() +'.ma'
+
+    pm.select(animClip)
+    pm.mel.source( "doExportClipArgList.mel" )
+    pm.mel.clipEditorExportClip( exportPath, "ma")
+
+# 파일저장
+exportPath = outputFoler_motionCapture +'/'+ pm.mel.basenameEx( mocapSrcFile ) +'.ma'
+
+exportNodes = pm.ls( type=['joint','character', 'HIKCharacterNode'])
+pm.select( exportNodes ) 
+pm.exportSelected( exportPath, f=True )
+
+#---------------
+#
+# playblast
+#
+#---------------
+
+#---------------
+# 그리드 생성
+#---------------
+#그리드 하나당 1m
+gridScale=1000 
+
+# 프리뷰 그리드 생성 : 얇은 줄
+mesh = pm.polyPlane(sh=10,sw=10)[0]
+mesh.s.set(gridScale,gridScale,gridScale) #그리드 하나당 1m
+mesh.getShape().overrideEnabled.set(True)
+mesh.getShape().overrideDisplayType.set(1) # 템플릿
+mesh.getShape().overrideShading.set(0)
+pm.polyDelEdge( [mesh.e[11], mesh.e[32], mesh.e[53], mesh.e[74], mesh.e[95], mesh.e[105], mesh.e[107], mesh.e[109], mesh.e[111], mesh.e[113], mesh.e[115:117], mesh.e[119], mesh.e[121], mesh.e[123], mesh.e[137], mesh.e[158], mesh.e[179], mesh.e[200]], cv=True, ch=1 )
+
+# 프리뷰 그리드 생성 : 중앙 굵은 줄
+mesh2 = pm.polyPlane(sh=2,sw=2,h=1.01,w=1.01)[0]
+mesh2.s.set(gridScale,gridScale,gridScale) #그리드 하나당 1m
+mesh2.getShape().overrideEnabled.set(True)
+mesh2.getShape().overrideDisplayType.set(2) # 레퍼런스
+mesh2.getShape().overrideShading.set(0)
+
+# 하나로 합치고 이름 변경
+pm.parent( mesh2.getShape(), mesh, s=True, r=True )
+pm.delete( mesh2 )   
+mesh.rename('previewGrid_10mx10m')
+
+# 위치 조정
+sampleJoints = pm.ls(type='joint')
+if sampleJoints:
+    pm.delete( pm.pointConstraint(sampleJoints,mesh) )
+    mesh.ty.set(0)
+
+#---------------
+# follow cam 생성
+#---------------
+# 카메라와 그룹 생성
+cam = pm.camera(n='followCam')[0]
+grp = pm.group( cam, n='followCam_grp' )
+
+# 컨스트레인
+pm.pointConstraint( pm.ls(type='joint'), grp ) # 컨스트레인
+
+# 카메라 조정
+cam.tz.set(1200)
+cam.focalLength.set(100)
+cam.centerOfInterest.set(1200)
+cam.filmFit.set(2) # vertical
+
+cam.r.set(-15, 15, 0)
+
+# 패널 조정
+activePanel = pm.playblast( activeEditor=True ).split('|')[-1]
+pm.modelPanel( activePanel, edit=True, camera=cam )
+
+# 카메라 핏 시킴
+pm.select( pm.ls(type='joint') )
+pm.viewFit( cam, fitFactor=1 )
+pm.select(cl=True)
+
+#---------------
+# 플레이 블라스트
+#---------------
+pm.playblast( 
+    #filename="D:/Users/kyuho_choi/Desktop/testssss.mov",
+    filename= outputFolder_playblast +'/'+ pm.mel.basenameEx( mocapSrcFile ) +'.mov',
+    forceOverwrite=True,
+
+    format='qt',
+    compression="H.264",
+    quality=80,
+    sequenceTime=0,
+
+    clearCache=True,
+    viewer=True,
+    showOrnaments=True,
+    offScreen=True,
+    fp=4,
+
+    widthHeight=[512,512],
+    percent=100,
+    )
